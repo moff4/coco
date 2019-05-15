2dbd55639325fbae948875e8678641429c5e71ff
Komissarov Andrey <komissar.off.andrey@gmail.com>
2019-05-15 21:17:34 +0300

    upd vk-api

--- snc/vk_api.py
+++ snc/vk_api.py
@@ -64,3 +64,23 @@ class VKAPI(AbstractApi):
                 'offset': offset,
             }
         )
+
+    def users_get(self, user_ids, fields=None):
+        """
+            possible fields:
+            photo_id, verified, sex, bdate, city, country, home_town, has_photo, photo_50,
+            photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig,
+            online, domain, has_mobile, contacts, site, education, universities, schools,
+            status, last_seen, followers_count, common_count, occupation, nickname,
+            relatives, relation, personal, connections, exports, activities, interests,
+            music, movies, tv, books, games, about, quotes, can_post, can_see_all_posts,
+            can_see_audio, can_write_private_message, can_send_friend_request, is_favorite,
+            is_hidden_from_feed, timezone, screen_name, maiden_name, crop_photo, is_friend,
+            friend_status, career, military, blacklisted, blacklisted_by_me
+        """
+        args = {
+            'user_ids': user_ids,
+        }
+        if fields is not None:
+            args['fields'] = fields
+        return self._request('users.get', args)
