b3e034aaeedabd1ddc5c515ba2f1f89ca54fca9e
Komissarov Andrey <komissar.off.andrey@gmail.com>
2019-05-16 17:16:10 +0300

    refactor

--- generator/mutation.py
+++ generator/mutation.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from functools import reduce
 
 
 class MutationType:
@@ -36,63 +37,45 @@ class Letter(Mutation):
         super(Mutation, self).__init__(MutationType.Word)
 
     def mutate(self, string):
-        mutations = []
-        for letter in string.lower():
-            if letter == "a":
-                mutations.append(string.replace('a', "@"))
-                mutations.append(string.replace('A', "@"))
-                mutations.append(string.replace('A', "@").replace('a','@'))
-            elif letter == "s":
-                mutations.append(string.replace('s', "$"))
-                mutations.append(string.replace('S', "$"))
-                mutations.append(string.replace('s', "$").replace('S','$'))
+        return reduce(
+            lambda x, y: x + y,
+            (
+                [
+                    string.replace('a', '@'),
+                    string.replace('A', '@'),
+                    string.replace('A', '@').replace('a', '@'),
+                ] if letter == 'a' else [
+                    string.replace('s', '$'),
+                    string.replace('S', '$'),
+                    string.replace('s', '$').replace('S', '$'),
+                ]
+                for letter in string.lower()
+                if letter in {'a', 's'}
+            )
+        )
 
 
 class MonthMutate(Mutation):
+    mutation_cases = {
+        '01': ['Jan', '1'],
+        '02': ['Feb', '2'],
+        '03': ['March', 'Mart', '3'],
+        '04': ['April', 'aprl', '4'],
+        '05': ['May', '5'],
+        '06': ['June', '6'],
+        '07': ['July', '7'],
+        '08': ['Aug', '8'],
+        '09': ['Sept', 'Sent', '9'],
+        '10': ['Oct', 'October', '10'],
+        '11': ['Nov', 'November', '11'],
+        '12': ['Dec', 'December', '12'],
+    }
+
     def __init__(self):
         super(Mutation, self).__init__(MutationType.Lexical)
 
     def mutate(self, string):
-        mutations = []
-        string = string.lower()
-        if string == "01":
-            mutations.append("Jan")
-            mutations.append("1")
-        elif string == "02":
-            mutations.append("Feb")
-            mutations.append("2")
-        elif string == "03":
-            mutations.append("March")
-            mutations.append("Mart")
-            mutations.append("3")
-        elif string == "04":
-            mutations.append("April")
-            mutations.append("Aprl")
-            mutations.append("4")
-        elif string == "05":
-            mutations.append("May")
-            mutations.append("5")
-        elif string == "06":
-            mutations.append("June")
-            mutations.append("6")
-        elif string == "07":
-            mutations.append("July")
-            mutations.append("7")
-        elif string == "08":
-            mutations.append("Aug")
-            mutations.append("8")
-        elif string == "09":
-            mutations.append("Sept")
-            mutations.append("Sent")
-            mutations.append("9")
-        elif string == "10":
-            mutations.append("Oct")
-            mutations.append("October")
-        elif string == "11":
-            mutations.append("Nov")
-            mutations.append("November")
-        else:
-            mutations.append("Dec")
-            mutations.append("December")
+        return self.mutation_cases.get(string, [])
+
 
 all_mutations = [Upper, Letter, MonthMutate]
