7163b5cdf90706904da196aea3d379c32cebe4bf
Komissarov Andrey <komissar.off.andrey@gmail.com>
2019-05-23 19:05:49 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='k2',
+    version='0.9.5',
+    author='Komissarov Andrey',
+    author_email='Komissar.off.andrey@mail.ru',
+    description='Password brutter',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/moff4/coco',
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
